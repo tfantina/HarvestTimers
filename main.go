541381647fe5de6721ab6be8cda8b801df2dea8d
Travis Fantina <tfantina@gmail.com>
2026-01-20 15:04:19 -0500

    Addsd daily and weekly tallies
    This expands the number of queries to 6 which is a lot but Harvest
    doesn't seem to have a good endpoint to get multiple times (even
    showing a breakdown of the week) I will investigate further.
    
    This is also getting a bit verbose, I could likely refactor with some
    more clever functions but since this is a personal tool, it does what I
    need and I'm not interested in tinkering with it a bunch.

--- main.go
+++ main.go
@@ -83,6 +83,10 @@ func (c *HarvestClient) GetTimeReport(from, to string) (*TeamMember, error) {
 
 	idx := slices.IndexFunc(report.Results, func(r TeamMember) bool { return r.UserName == "Travis Fantina" })
 
+	if idx < 0 {
+		return nil, nil
+	}
+
 	return &report.Results[idx], nil
 }
 
@@ -91,6 +95,22 @@ func getCurrentMonthDateRange() (string, string) {
 	startOfMonth := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
 
 	return startOfMonth.Format("20060102"), now.Format("20060102")
+}
+
+func getCurrentWeek() (string, string) {
+	now := time.Now()
+	startOfWeek := now
+
+	for startOfWeek.Weekday() != 1 {
+		startOfWeek = startOfWeek.AddDate(0, 0, -1)
+	}
+
+	return startOfWeek.Format("20060102"), now.Format("20060102")
+}
+
+func getCurrentDay() string {
+	now := time.Now()
+	return now.Format("20060102")
 }
 
 func main() {
@@ -111,25 +131,56 @@ func main() {
 	client1 := NewHarvestClient(accessToken, accountID)
 	client2 := NewHarvestClient(accessToken2, accountID2)
 
-	from, to := getCurrentMonthDateRange()
-	fmt.Printf("Fetching expenses from %s to %s\n", from, to)
+	monthFrom, monthTo := getCurrentMonthDateRange()
+	weekFrom, weekTo := getCurrentWeek()
+	day := getCurrentDay()
 
-	hours1, err1 := client1.GetTimeReport(from, to)
-	hours2, err2 := client2.GetTimeReport(from, to)
+	hours1, err1 := client1.GetTimeReport(monthFrom, monthTo)
+	hours2, err2 := client2.GetTimeReport(monthFrom, monthTo)
+	weekHours1, err3 := client1.GetTimeReport(weekFrom, weekTo)
+	weekHours2, err4 := client2.GetTimeReport(weekFrom, weekTo)
+	dayHours1, err5 := client1.GetTimeReport(day, day)
+	dayHours2, err6 := client2.GetTimeReport(day, day)
 
-	if err1 != nil || err2 != nil {
+	fmt.Printf("%s", weekFrom)
+	fmt.Print(weekTo)
+
+	var errors = []error{err1, err2, err3, err4, err5, err6}
+
+	if !slices.Contains(errors, nil) {
+		fmt.Print(errors)
 		log.Fatalf("Error fetching expense report")
 	}
 
 	var results = []*TeamMember{hours1, hours2}
+	var weekResults = []*TeamMember{weekHours1, weekHours2}
+	var dayResults = []*TeamMember{dayHours1, dayHours2}
 
 	var grandTotal float64
+	var grandTotalWeek float64
+	var grandTotalDay float64
 
 	for _, result := range results {
 		if result.TotalHours > 0 {
 			grandTotal += result.TotalHours
 		}
 	}
+
+	for _, result := range weekResults {
+		if result != nil && result.TotalHours > 0 {
+			grandTotalWeek += result.TotalHours
+		}
+	}
+
+	for _, result := range dayResults {
+		if result != nil && result.TotalHours > 0 {
+			grandTotalDay += result.TotalHours
+		}
+	}
+
+	fmt.Printf("Hours Today: %.2f\n", grandTotalDay)
+	fmt.Printf("Hours Week: %.2f\n", grandTotalWeek)
+	fmt.Printf("----------\n")
 	fmt.Printf("Objective %.2f\n", hours1.TotalHours)
 	fmt.Printf("Built %.2f\n", hours2.TotalHours)
 	fmt.Printf("\nGrand Total: %.2f\n", grandTotal)
